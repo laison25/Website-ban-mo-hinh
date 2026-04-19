c09141eddc96d1b7c2abfeb26a7863e29f36fab0
VinhNguyen25 <nguyenta1234www@gmail.com>
2026-04-19 23:03:00 +0700

    cập nhật code

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 const APP_NAME = 'Website Bán Mô Hình';
-const APP_URL  = 'http://localhost/website-ban-mo-hinh-php-v3/';
+const APP_URL  = 'http://localhost:8888/Website-ban-mo-hinh/';
 
 const DB_HOST = '127.0.0.1';
-const DB_PORT = 3306;
+const DB_PORT = 3307;
 const DB_NAME = 'website_ban_mo_hinh';
 const DB_USER = 'root';
 const DB_PASS = '';
